5c4f4544dbb1923ca89946c3801703170f3cf3b1
Rob Yedlin <ryedlin@gmail.com>
2025-12-02 17:07:51 -0500

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,15 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['/tests'],
-  testMatch: ['**/*.test.ts'],
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.d.ts',
-    '!src/**/*.test.ts',
-  ],
+  roots: ['./tests'],
+  testMatch: ['**/__tests__/**/*.ts', '**/*.test.ts', '**/*.spec.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
   coverageThreshold: {
     global: {
       branches: 80,
       functions: 80,
       lines: 80,
-      statements: 80,
-    },
-  },
-};
+      statements: 80
+    }
+  }
+}
